292da2f29ec0f6420311e370b659396a83d1f2d3
fcrios145 <fcrios145@hotmail.com>
2014-03-12 22:19:40 +0000

    Empezar con la pagina de inicio

--- jump_project/settings.py
+++ jump_project/settings.py
@@ -36,7 +36,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #aplicaciones instaladas
     'south',
+    'djfrontend',
+    'djfrontend.skeleton',
+    #Modelos
     'noticias'
 )
 
--- noticias/views.py
+++ noticias/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 
 # Create your views here.
+
+def index(request):
+    data = {'data'}
+    return render_to_response('noticias/home.html', {'data': data})
